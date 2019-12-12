98ae3d73968d3d421145769638ccbb51a945819b
Eduard Kizmann <kizmann@protonmail.ch>
2019-12-12 01:10:55 +0100

    *

--- src/library/extension.js
+++ src/library/extension.js
@@ -4,8 +4,6 @@ export class Extension
 {
     static imports = Obj.get(window, '_imports', {});
 
-    static exports = {};
-
     static pending = [];
 
     static loaded = [];
@@ -13,87 +11,68 @@ export class Extension
     static aborted = [];
 
 
-    static bind (name, config)
+    static bind(name, config)
     {
         this.imports[name] = Obj.assign({
-            scripts: [], styles: [], modules: []
+            scripts: [], styles: []
         }, config);
     }
 
-    static load (name, load = undefined, error = undefined)
+    static get(name, done = null, error = null)
     {
-        let reload = () => {
-            this.load(name, load, error);
-        };
-
-        if ( ! Obj.has(this.imports, name) ) {
-            return error ? error() : error;
-        }
-
-        if ( Obj.has(this.loaded, name) ) {
-            return load ? load() : load;
-        }
-
-        if ( Obj.has(this.aborted, name) ) {
-            return error ? error() : error;
-        }
-
-        if ( Obj.has(this.pending, name) ) {
-            return setTimeout(reload, 100);
+        if ( this.imports[name] === undefined ) {
+            return error ? error() : console.error('Import ' + name + ' not found.');
         }
 
         let queue = new Queue();
 
-        queue.add((next) => {
-            this.pending.push(name);
-            next();
-        });
-
         Arr.each(this.imports[name].styles, (style) => {
-            queue.add((next) => Asset.style(style, next, error));
+            queue.add((next) => {
+
+                if ( Arr.has(this.loaded, style) ) {
+                    return next();
+                }
+
+                this.pending.push(style);
+
+                let loaded = () => {
+                    this.loaded.push(style); next();
+                };
+
+                let aborted = () => {
+                    this.aborted.push(style); next();
+                };
+
+                Asset.style(style, loaded, aborted);
+            });
         });
 
         Arr.each(this.imports[name].scripts, (script) => {
-            queue.add((next) => Asset.script(script, next, error));
+            queue.add((next) => {
+
+                if ( Arr.has(this.loaded, script) ) {
+                    return next();
+                }
+
+                this.pending.push(script);
+
+                let loaded = () => {
+                    this.loaded.push(script); next();
+                };
+
+                let aborted = () => {
+                    this.aborted.push(script); next();
+                };
+
+                Asset.script(script, loaded, aborted);
+            });
         });
 
-        queue.add((next) => {
-            this.pending = Arr.diff(this.pending, name);
-            next();
-        });
-
-        queue.add((next) => {
-            this.loaded.push(name);
-            next();
-        });
-
-        queue.add(load).run();
-    };
-
-    static export (name, data)
-    {
-        return this.exports[name] = data;
-    }
-
-    static import (name, load = null, error = null)
-    {
-        if ( this.exports[name] !== undefined ) {
-            return load ? load(this.exports[name]) : load;
+        if ( typeof done === "function" ) {
+            queue.add(done);
         }
 
-        let index = Obj.findIndex(this.imports, (item) => {
-            return Arr.intersect(item.modules, [name]).length !== 0;
-        });
-
-        if ( index === undefined ) {
-            return error ? error() : console.error('Import ' + name + ' not found.');
-        }
-
-        if ( this.loaded[index] !== undefined ) {
-            return load ? load(this.exports[name]) : load;
-        }
-
-        this.load(index, () => load(this.exports[name]), error);
+        queue.run();
     };
 
 }
