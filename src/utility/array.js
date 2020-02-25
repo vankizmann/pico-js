175caee6d490bd1643acf7ae1a3967da5c57c2d4
Eduard Kizmann <kizmann@protonmail.ch>
2020-02-25 22:57:24 +0100

    *

--- src/utility/array.js
+++ src/utility/array.js
@@ -68,7 +68,7 @@ export class Arr
 
         if ( ! Any.isFunction(key) ) {
             keys = keys.sort((a, b) => {
-                return Any.integer(key(obj[a])) - Any.integer(key(obj[b]));
+                return Any.integer(Obj.get(obj[a], key)) - Any.integer(Obj.get(obj[b], key));
             })
         }
 
