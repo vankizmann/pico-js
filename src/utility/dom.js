1344014bdda31dd503319824476998a225c2af7f
Eduard Kizmann <kizmann@protonmail.ch>
2021-04-22 13:22:33 +0200

    *

--- src/utility/dom.js
+++ src/utility/dom.js
@@ -64,7 +64,7 @@ export class Dom
         });
 
         if ( Arr.has(matches, false) ) {
-            setTimeout(() => Dom.ready(callback, globals, timer), timer);
+            setTimeout(() => Dom.required(callback, globals, timer), timer);
             return this;
         }
 
